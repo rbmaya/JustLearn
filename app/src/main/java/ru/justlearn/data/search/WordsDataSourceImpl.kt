7d46bab998ad307f76193ec5a023104a2a936a16
rmaya <maya.rybina@mail.ru>
2024-10-13 18:09:24 +0700

    join together words with the same value

--- app/src/main/java/ru/justlearn/data/search/WordsDataSourceImpl.kt
+++ app/src/main/java/ru/justlearn/data/search/WordsDataSourceImpl.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import ru.justlearn.data.mappers.WordWebMapper
 import ru.justlearn.domain.Word
+import java.util.UUID
 import javax.inject.Inject
 
 class WordsDataSourceImpl @Inject constructor(
@@ -14,6 +15,20 @@ class WordsDataSourceImpl @Inject constructor(
         return withContext(Dispatchers.IO) {
             val response = dictionaryApi.searchWord(query)
             response.body()?.let { WordWebMapper.mapList(it) }
+                ?.groupBy { it.value }
+                ?.map { (value, words) ->
+                    Word(
+                        id = words.first().id,
+                        value = value,
+                        phonetics = words
+                            .map { it.phonetics }
+                            .flatten()
+                            .distinctBy { it.phonetic },
+                        meanings = words
+                            .map { it.meanings }
+                            .flatten(),
+                    )
+                }
                 .orEmpty()
         }
     }
